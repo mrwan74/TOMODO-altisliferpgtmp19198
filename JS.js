9edb4757cbde6e69ca47efaa3c116fbbfd64a8fd
mrwan74 <mrwan74@gmail.com>
2014-05-02 01:06:07 +0300

    a commit from TOMODO - JS File

---
+++ JS.js
@@ -0,0 +1,30 @@
+/**
+ * TOMODO
+ * 
+ * This file holds the user script written by YOU.
+ * It only appears in Preview mode and its purpose is to allow you to debug your script.
+ * Put your breakpoints below.
+ * 
+ * Thanks,
+ * the TOMODO Team.  
+ */
+
+function TOMDO_runUserScript(){
+	//----------------------------------- Your Code Here ------------------------------------	
+	/**********************************************
+Add your Javascript code below.
+
+To debug your code use Preview mode and
+debug tomodouserscript.js using
+Chrome Developer Tools, Firebug or another Javascript debugger.
+**********************************************/
+
+bi$('#category_3 > div.ipsBox > div.ipsBox_container > table.ipb_table > tbody > tr:nth-of-type(2) > td.col_c_forum:nth-of-type(2)').html( '<h4>FUCK YOU<br></h4>' );	
+	//---------------------------------------------------------------------------------------	
+}
+
+function TOMODO_runWYSIWYGScript(){
+	//----------------------------------- WYSIWYG Auto generated code -----------------------
+	
+	//---------------------------------------------------------------------------------------
+}
